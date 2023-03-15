ee4f2f48257fbf712341d1c595a46c61386064d8
o6ris <loic.tsiry@gmail.com>
2023-03-15 15:43:57 +0100

    route get all vehicles ok

--- backend/src/router.js
+++ backend/src/router.js
@@ -2,12 +2,14 @@ const express = require("express");
 
 const router = express.Router();
 
-const itemControllers = require("./controllers/itemControllers");
+const vehicleControllers = require("./controllers/vehicleControllers");
 
-router.get("/items", itemControllers.browse);
-router.get("/items/:id", itemControllers.read);
-router.put("/items/:id", itemControllers.edit);
-router.post("/items", itemControllers.add);
-router.delete("/items/:id", itemControllers.destroy);
+// router.get("/items", itemControllers.browse);
+// router.get("/items/:id", itemControllers.read);
+// router.put("/items/:id", itemControllers.edit);
+// router.post("/items", itemControllers.add);
+// router.delete("/items/:id", itemControllers.destroy);
+
+router.get("/vehicles", vehicleControllers.browse);
 
 module.exports = router;
