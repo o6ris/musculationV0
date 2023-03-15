116c35d013946621c8d431c31b375bc8b31a941b
o6ris <loic.tsiry@gmail.com>
2023-03-15 16:04:04 +0100

    route delete ok

--- backend/src/router.js
+++ backend/src/router.js
@@ -4,15 +4,10 @@ const router = express.Router();
 
 const vehicleControllers = require("./controllers/vehicleControllers");
 
-// router.get("/items", itemControllers.browse);
-// router.get("/items/:id", itemControllers.read);
-// router.put("/items/:id", itemControllers.edit);
-// router.post("/items", itemControllers.add);
-// router.delete("/items/:id", itemControllers.destroy);
-
 router.get("/vehicles", vehicleControllers.browse);
 router.get("/vehicles/:id", vehicleControllers.read);
 router.post("/vehicles", vehicleControllers.add);
 router.put("/vehicles/:id", vehicleControllers.edit);
+router.delete("/vehicles/:id", vehicleControllers.destroy);
 
 module.exports = router;
