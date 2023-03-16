3500ba3819a3365dedb9bf98e1866ee60b415f86
o6ris <loic.tsiry@gmail.com>
2023-03-16 09:22:05 +0100

    display les datas

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,43 @@
+import React, { useEffect, useState } from "react";
+import apiConnection from "@services/apiConnection";
+
 export default function Home() {
+  const [vehicles, setVehicles] = useState();
+
+  const getAllVehicles = () => {
+    apiConnection
+      .get(`/vehicles`)
+      .then((allVehicles) => setVehicles(allVehicles.data))
+      .catch((error) => console.error(error));
+  };
+
+  useEffect(() => {
+    getAllVehicles();
+  }, []);
   return (
     <header className="App-header">
-      <p>Test husky!</p>
+      <h1>Rent my car</h1>
+      <section>
+        <ul>
+          {vehicles?.map((vehicle) => (
+            <li>
+              <div>
+                <h2>{vehicle.name}</h2>
+                <img src={vehicle.picture} alt={vehicle.name} />
+                <ul>
+                  {vehicle.fuel === 0 ? <li>Essence</li> : <li>Diesel</li>}
+                  {vehicle.gearbox === 0 ? (
+                    <li>Manuel</li>
+                  ) : (
+                    <li>Automatique</li>
+                  )}
+                </ul>
+                <h3>{vehicle.price} €</h3>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </section>
     </header>
   );
 }
