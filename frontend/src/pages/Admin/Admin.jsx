eba7b49357d288e7bd52363a8db8743c78375544
o6ris <loic.tsiry@gmail.com>
2023-03-16 18:18:18 +0100

    update vehicle ok

--- frontend/src/pages/Admin/Admin.jsx
+++ frontend/src/pages/Admin/Admin.jsx
@@ -4,6 +4,7 @@ import apiConnection from "@services/apiConnection";
 import "./adminStyle.css";
 
 function Admin() {
+  const [displayForm, setDisplayForm] = useState(false);
   const [vehicle, setVehicle] = useState({
     name: "",
     picture: "",
@@ -26,50 +27,67 @@ function Admin() {
   const getOneVehicle = (id) => {
     apiConnection
       .get(`/vehicles/${id}`)
-      .then((oneVehicle) => setVehicle(oneVehicle.data))
+      .then((oneVehicle) => {
+        setDisplayForm(true);
+        setVehicle(oneVehicle.data);
+      })
       .catch((err) => console.error(err));
   };
+
+  const handleEditVehicle = () => {
+    apiConnection
+      .put(`/vehicles/${vehicle?.id}`, vehicle)
+      .then(() => {
+        setDisplayForm(false);
+      })
+      .catch((err) => console.error(err));
+  };
+
   return (
     <div>
       <AdminSearchBar getOneVehicle={getOneVehicle} />
-      <form className="editVehicleContainer">
-        <input
-          type="text"
-          name="name"
-          value={vehicle?.name}
-          onChange={(e) => handleInputOnChange(e.target.name, e.target.value)}
-        />
-        <input
-          type="text"
-          name="picture"
-          value={vehicle?.picture}
-          onChange={(e) => handleInputOnChange(e.target.name, e.target.value)}
-        />
-        <select
-          onChange={(e) => handleInputOnChange(e.target.name, e.target.value)}
-          name="fuel"
-          value={vehicle.fuel}
-        >
-          <option value={0}>Essence</option>
-          <option value={1}>Diesel</option>
-        </select>
-        <select
-          onChange={(e) => handleInputOnChange(e.target.name, e.target.value)}
-          name="gearbox"
-          value={vehicle.gearbox}
-        >
-          <option value={0}>Manual</option>
-          <option value={1}>Automatic</option>
-        </select>
-        <input
-          type="text"
-          name="price"
-          value={vehicle?.price}
-          onChange={(e) => handleInputOnChange(e.target.name, e.target.value)}
-        />
-        <button type="button">Update</button>
-        <button type="button">Cancel</button>
-      </form>
+      {displayForm && (
+        <form className="formVehicleContainer">
+          <input
+            type="text"
+            name="name"
+            value={vehicle?.name}
+            onChange={(e) => handleInputOnChange(e.target.name, e.target.value)}
+          />
+          <input
+            type="text"
+            name="picture"
+            value={vehicle?.picture}
+            onChange={(e) => handleInputOnChange(e.target.name, e.target.value)}
+          />
+          <select
+            onChange={(e) => handleInputOnChange(e.target.name, e.target.value)}
+            name="fuel"
+            value={vehicle.fuel}
+          >
+            <option value={0}>Essence</option>
+            <option value={1}>Diesel</option>
+          </select>
+          <select
+            onChange={(e) => handleInputOnChange(e.target.name, e.target.value)}
+            name="gearbox"
+            value={vehicle.gearbox}
+          >
+            <option value={0}>Manual</option>
+            <option value={1}>Automatic</option>
+          </select>
+          <input
+            type="text"
+            name="price"
+            value={vehicle?.price}
+            onChange={(e) => handleInputOnChange(e.target.name, e.target.value)}
+          />
+          <button onClick={handleEditVehicle} type="button">
+            Update
+          </button>
+          <button type="button">Cancel</button>
+        </form>
+      )}
     </div>
   );
 }
